4e0f9a683845cfef0ee510881c8565c456bb166d
鸡肉爱土豆 <139844998+exo101@users.noreply.github.com>
2026-01-26 17:38:48 +0800

    Delete heartlib directory

--- heartlib/src/heartlib/__init__.py
+++
@@ -1,7 +0,0 @@
-from .pipelines.music_generation import HeartMuLaGenPipeline
-from .pipelines.lyrics_transcription import HeartTranscriptorPipeline
-
-__all__ = [
-    "HeartMuLaGenPipeline",
-    "HeartTranscriptorPipeline"
-]
